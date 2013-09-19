edca2188c39b11428e4df3c64105e639d78bfd19
Alan Koda <alankoda@gmail.com>
2013-09-19 12:45:20 -0500

    Debug works display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_var
+  
+  private
+  
+  def set_var
+    @works = Work.all
+  end
 
 end
